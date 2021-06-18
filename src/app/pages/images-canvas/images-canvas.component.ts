e9f9d47394b1f60b74328bf43e0a37eb6c3baba1
LAIDANI Imane <Imane.LAIDANI@akka.eu>
2021-06-18 17:16:28 +0200

    Adding canvas component

---
+++ src/app/pages/images-canvas/images-canvas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-canvas',
+  templateUrl: './images-canvas.component.html',
+  styleUrls: ['./images-canvas.component.scss']
+})
+export class CanvasComponent {
+ 
+
+}
