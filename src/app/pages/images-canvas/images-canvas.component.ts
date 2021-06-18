9de8f542ad8dcfe3d52ba51b22c7ecdb206798ee
LAIDANI Imane <Imane.LAIDANI@akka.eu>
2021-06-18 17:29:40 +0200

    add some text to the canvas

--- src/app/pages/images-canvas/images-canvas.component.ts
+++ src/app/pages/images-canvas/images-canvas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 
 @Component({
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./images-canvas.component.scss']
 })
 export class CanvasComponent {
- 
+  @ViewChild('myCanvas', {static: false}) canvasEl!: ElementRef;
+  public context: CanvasRenderingContext2D | any;
+  scrHeight: number =  500;
+  scrWidth: number = 500;
 
+
+  ngAfterViewInit(): void {
+    this.context = this.canvasEl.nativeElement.getContext('2d');
+    this.draw();
+  }
+
+   /**
+   * Just some code to test the canvas
+   */
+    private draw() {
+      this.context.font = "30px Arial";
+      this.context.textBaseline = 'middle';
+      this.context.textAlign = 'center';
+  
+      const x = (this.canvasEl.nativeElement as HTMLCanvasElement).width / 2;
+      const y = (this.canvasEl.nativeElement as HTMLCanvasElement).height / 2;
+      this.context.fillText("AKKA", x, y);
+  
+  }
 }
