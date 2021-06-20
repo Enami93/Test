0b2881c5c362be8dff2a07f08402016cd87770be
LAIDANI Imane <Imane.LAIDANI@akka.eu>
2021-06-20 11:06:57 +0200

    Adding file reader

--- src/shared/services/electron.services.ts
+++ src/shared/services/electron.services.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 
-import { ipcRenderer, webFrame } from 'electron';
+import { ipcRenderer, webFrame, ipcMain} from 'electron';
 import * as remote from '@electron/remote';
 import * as childProcess from 'child_process';
 import * as fs from 'fs';
@@ -13,6 +13,7 @@ export class ElectronService {
 
   
   ipcRenderer: typeof ipcRenderer;
+  ipcMain: typeof ipcMain;
   webFrame: typeof webFrame;
   remote: typeof remote;
   childProcess: typeof childProcess;
@@ -26,6 +27,7 @@ export class ElectronService {
     // Conditional imports
     if (this.isElectron) {
       this.ipcRenderer = window.require('electron').ipcRenderer;
+      this.ipcMain = window.require('electron').ipcMain;
       this.webFrame = window.require('electron').webFrame;
 
       // If you want to use remote object in renderer process, please set enableRemoteModule to true in main.ts
@@ -37,15 +39,32 @@ export class ElectronService {
     }
   }
   loadImage() {
-    this.remote.dialog.showOpenDialog({
-      properties: ['openFile', 'multiSelections']
-  }, function (files) {
-      if (files !== undefined) {
-          // handle files
-      }
-  });
+    console.log("here", this.ipcRenderer);
+      this.remote.dialog.showOpenDialog({
+        properties: ['openFile'], 
+        filters: [{ name: 'Images', extensions: ['jpg', 'png', 'gif']}]
+      }).then(result => {
+        console.log(result.canceled)
+        console.log(result.filePaths)
+        this.fs.readFile(result.filePaths[0], function(err, data) { 
+          console.log(data)
+      })
+    
+      })
+    }
+}
 
-  }
+    // Change how to handle the file content
+  
+  // (files) => {
+  //   console.log(files);
+  //       if(!files) return; 
+  //       const file = files[0];
+  //       this.fs.readFile(file, (err, data) => {
+  //         console.log("Asynchronous read: " + data.toString());
+  //       })
+  //      });
 
   
-}
+
+  
