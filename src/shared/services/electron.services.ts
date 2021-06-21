b093b242390de7cdae7a76d98bc9a06953619612
LAIDANI Imane <Imane.LAIDANI@akka.eu>
2021-06-21 14:58:29 +0200

    zoom in zoom out + comments + readme update

--- src/shared/services/electron.services.ts
+++ src/shared/services/electron.services.ts
@@ -1,18 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-
-import { ipcRenderer, webFrame, ipcMain} from 'electron';
-import * as remote from '@electron/remote';
-import * as childProcess from 'child_process';
-import * as fs from 'fs';
-import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
+import { ipcRenderer, webFrame, ipcMain } from "electron";
+import * as remote from "@electron/remote";
+import * as childProcess from "child_process";
+import * as fs from "fs";
+import { BehaviorSubject, Observable, of, Subject } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ElectronService {
-
-  
   ipcRenderer: typeof ipcRenderer;
   ipcMain: typeof ipcMain;
   webFrame: typeof webFrame;
@@ -27,36 +24,35 @@ export class ElectronService {
   constructor() {
     // Conditional imports
     if (this.isElectron) {
-      this.ipcRenderer = window.require('electron').ipcRenderer;
-      this.ipcMain = window.require('electron').ipcMain;
-      this.webFrame = window.require('electron').webFrame;
+      this.ipcRenderer = window.require("electron").ipcRenderer;
+      this.ipcMain = window.require("electron").ipcMain;
+      this.webFrame = window.require("electron").webFrame;
 
-      // If you want to use remote object in renderer process, please set enableRemoteModule to true in main.ts
-      this.remote = window.require('@electron/remote');
-      // console.log('remote - globalShortcut', this.remote.globalShortcut);
+      // If you want to use remote object in renderer process
+      this.remote = window.require("@electron/remote");
 
-      this.childProcess = window.require('child_process');
-      this.fs = window.require('fs');
+      this.childProcess = window.require("child_process");
+      this.fs = window.require("fs");
     }
   }
   /**
    * load only image and update value of selected image
+   * return a subject with the selected image (buffer)
    */
-  loadImage(selectedFile: Subject<any>){
-  //  console.log(this.remote.dialog);
-   return this.remote.dialog.showOpenDialog({
-      properties: ['openFile'],
-      filters: [{ name: 'Images', extensions: ['jpg', 'png', 'gif'] }]
-    }).then(data => {
-        this.fs.readFile(data.filePaths[0],  {encoding: 'base64'}, function (err, data) {
-        selectedFile.next(data);
-       });
-
-    });
+  loadImage(selectedFile: Subject<any>) {
+    return this.remote.dialog
+      .showOpenDialog({
+        properties: ["openFile"],
+        filters: [{ name: "Images", extensions: ["jpg", "png", "gif"] }],
+      })
+      .then((data) => {
+        this.fs.readFile(
+          data.filePaths[0],
+          { encoding: "base64" },
+          function (err, data) {
+            selectedFile.next(data);
+          }
+        );
+      });
   }
-
 }
-
-  
-
-  
